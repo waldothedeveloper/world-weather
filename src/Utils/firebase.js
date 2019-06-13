b1db3e6ddb5d54d2e9ba0a1486fb31e4c4d38a4a
Waldo Lavaut <waldothedeveloper@gmail.com>
2019-06-13 14:36:01 -0400

    taking out the api keys from firebase

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBHiN4e-KQ-dDRdz1foLdjU6XHC6rzZ5Y8",
-  authDomain: "weatherkast-waldothedev.firebaseapp.com",
-  databaseURL: "https://weatherkast-waldothedev.firebaseio.com",
-  projectId: "weatherkast-waldothedev",
-  storageBucket: "weatherkast-waldothedev.appspot.com",
-  messagingSenderId: "721606499992",
-  appId: "1:721606499992:web:ffca15e6457d6dd1"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.DATABASEURL,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID
 };
 
 export default firebase.initializeApp(firebaseConfig);
